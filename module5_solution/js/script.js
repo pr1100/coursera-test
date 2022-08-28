70c40add3ae6338d35f72c9722273003752cfce9
parshuamber <parshu@amber.ag>
2022-08-28 12:17:23 -0500

    assignment5 sol

--- module5_solution/js/script.js
+++ module5_solution/js/script.js
@@ -27,7 +27,7 @@ var menuItemHtml = "snippets/menu-item.html";
 var insertHtml = function (selector, html) {
   var targetElem = document.querySelector(selector);
   targetElem.innerHTML = html;
-  console.log(`Parshu 6 ${selector} ${html} ${targetElem}`);
+  console.log(`Parshu 6 ${selector} ${html} ${targetElem.innerHTML}`);
 };
 
 // Show loading icon inside element identified by 'selector'.
